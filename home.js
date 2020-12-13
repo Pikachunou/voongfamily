dbcf5d29f4146cfa7b1e3e189e221608473209d3
Pikachunou <peterstar.voo@gmail.com>
2020-12-13 12:02:10 -0800

    trying to remove logout

--- home.js
+++ home.js
@@ -8,10 +8,10 @@ firebase.auth().onAuthStateChanged(function (user) {
         window.location.href="/index.html"
     }
 });
-  document.querySelector(".logout-button").addEventListener("click", function(){
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
-  })
+  // document.querySelector(".logout-button").addEventListener("click", function(){
+  //   firebase.auth().signOut().then(function() {
+  //       // Sign-out successful.
+  //     }).catch(function(error) {
+  //       // An error happened.
+  //     });
+  // })
