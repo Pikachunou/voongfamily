5ced9ecde03410d9ee26601655d01bd0c85dea9d
pikachunou <peterstar.voo@gmail.com>
2023-10-06 11:35:06 -0700

    added uploading

--- home.js
+++ home.js
@@ -15,3 +15,39 @@
 //   //       // An error happened.
 //   //     });
 //   // })
+function uploadImage() {
+    const fileInput = document.getElementById('imageInput');
+    const file = fileInput.files[0];
+  
+    if (file) {
+      // Create a reference to the storage location
+      const storageRef = storage.ref('images/' + file.name);
+  
+      // Upload the file to Firestore Storage
+      const uploadTask = storageRef.put(file);
+  
+      // Monitor the upload progress
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          // Handle progress (e.g., show a progress bar)
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+        }, 
+        (error) => {
+          // Handle errors
+          console.error('Error uploading image: ', error);
+        }, 
+        () => {
+          // Handle successful upload
+          console.log('Image uploaded successfully');
+  
+          // You can retrieve the download URL to store in Firestore or use in your application
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            // Now you can store this URL in Firestore or use it as needed.
+          });
+        }
+      );
+    }
+  }
+  
--- index.js
+++ index.js
@@ -11,6 +11,21 @@
 //     var errorMessage = error.message;
 //     // ...
 //   });
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    FIREBASE_CONFIGURATION
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault()
@@ -42,3 +57,4 @@ firebase.auth().onAuthStateChanged(function (user) {
         
     }
 });
+
